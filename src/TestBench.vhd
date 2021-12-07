f48af3c40d7da671253d9c566de3f51af02cb5a1
Sabet Maulana Mubarok <86289370+saabet@users.noreply.github.com>
2021-12-07 21:41:04 +0700

    Coding ke-4

--- src/TestBench.vhd
+++ src/TestBench.vhd
@@ -2,28 +2,73 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity testbenchTL is
-end testbench;
+end testbenchTL;
 
 architecture test of testbenchTL is
 	component controller is
 		port(
-			clr: in std_logic;
-			clk: in std_logic
+			clr : in std_logic;
+			clk : in std_logic;
+
+			North : out std_logic_vector(2 downto 0);
+			East  : out std_logic_vector(2 downto 0);
+			South : out std_logic_vector(2 downto 0);
+			West  : out std_logic_vector(2 downto 0)
 		);
 	end component;
 	
 	signal clk : std_logic := '0';
 	signal clr : std_logic := '0';
+
+	signal North : std_logic_vector(2 downto 0) := "000";
+	signal East  : std_logic_vector(2 downto 0) := "000";
+	signal South : std_logic_vector(2 downto 0) := "000";
+	signal West  : std_logic_vector(2 downto 0) := "000";
+
+	signal counter_clk : integer := 0;
 	constant clk_period : time := 1 ns;
 begin
 	
-	UUT : controller port map(clr, clk);
+	UUT : controller port map(clr, clk, North, East, South, West);
 	
-	clk_process :process
+	test_process : process
 	begin
 		clk <= '0';
 		wait for clk_period/2;  --for 0.5 ns signal is '0'.
 		clk <= '1';
+		counter_clk <= counter_clk + 1; --count the clock cycle.
 		wait for clk_period/2;  --for next 0.5 ns signal is '1'.
+		
+		if(counter_clk = 35) then
+			clr <= '1';
+			assert North = "001" report "Output tidak sesuai!" severity warning;
+			assert East  = "100" report "Output tidak sesuai!" severity warning;
+			assert South = "001" report "Output tidak sesuai!" severity warning;
+			assert West  = "001" report "Output tidak sesuai!" severity warning;
+		
+		elsif(counter_clk = 36) then
+			clr <= '0';
+			assert North = "010" report "Output tidak sesuai!" severity warning;
+			assert East  = "001" report "Output tidak sesuai!" severity warning;
+			assert South = "001" report "Output tidak sesuai!" severity warning;
+			assert West  = "010" report "Output tidak sesuai!" severity warning;
+
+		elsif(counter_clk = 45) then
+			clr <= '1';
+			assert North = "100" report "Output tidak sesuai!" severity warning;
+			assert East  = "001" report "Output tidak sesuai!" severity warning;
+			assert South = "001" report "Output tidak sesuai!" severity warning;
+			assert West  = "001" report "Output tidak sesuai!" severity warning;
+
+		elsif(counter_clk = 55) then
+			clr <= '0';
+			assert North = "010" report "Output tidak sesuai!" severity warning;
+			assert East  = "001" report "Output tidak sesuai!" severity warning;
+			assert South = "001" report "Output tidak sesuai!" severity warning;
+			assert West  = "010" report "Output tidak sesuai!" severity warning;
+
+		elsif(counter_clk = 100) then
+			wait;
+		end if;
 	end process;
-end test;
+end architecture;
--- src/Traffic Light v2.vhd
+++ src/Traffic Light v2.vhd
@@ -1,20 +1,23 @@
--- 4 way traffic light control
+-- 4 ways traffic light controller
 
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
--- port definition
--- clr: clears all outputs
--- green, yellow, red: lights in 4 ways E-W-N-S order
--- zebraRed, zebraGreen: zebra crossing lights EW-NS order
 
 entity controller is
 	port(
-		clr: in std_logic;
-		clk: in std_logic
+		clr : in std_logic; -- clear all outputs
+		clk : in std_logic;
+		
+		--output port for each direction
+		-- 100 = GREEN, 010 = YELLOW, 001 = RED
+		North : out std_logic_vector(2 downto 0);
+		East  : out std_logic_vector(2 downto 0);
+		South : out std_logic_vector(2 downto 0);
+		West  : out std_logic_vector(2 downto 0)
 	);
-end controller;
+end controller; 
 
 architecture arch of controller is
 
@@ -29,17 +32,19 @@ architecture arch of controller is
 	-- used in timer to generate delay
 	constant longCount   : integer := 20; --count 20 clock pulses
 	constant shortCount  : integer := 5; --count 5 clock pulses
-	signal count : integer := 1;
+	signal count : integer := 1; --counting the clock cycles
 
 	-- variable pre_status: integer := 0;
 	signal timeout   : std_logic := '0'; -- flag : '1' if timeout in any state
 	signal Tl, Ts    : std_logic := '0'; -- signals to trigger timer function : Tl - long time, Ts - short time
 	
+	--state declaration using type
 	type states is (S0, S1, S2, S3, S4, S5, S6, S7);
 	signal PS, NS : states;
 	
+	--lamp declaration using type
 	type tipe_lampu is (RED, GREEN, YELLOW);
-	signal N,W,E,S : tipe_lampu; -- N for North, E for East, S for South, W for West.
+	signal N,E,S,W : tipe_lampu; -- N for North, E for East, S for South, W for West.
 	
 	--signal inBCDRed_puluhan   : std_logic_vector(3 downto 0);  --input BCD Red
 	--signal inBCDRed_satuan    : std_logic_vector(3 downto 0);  --input BCD Red
@@ -71,14 +76,16 @@ begin --architecture
     comb: process (PS, count)
     begin
         Tl <= '0'; Ts <= '0';
-		if(clr = '1') then
+		if(clr = '1') then -- yang terjadi saat tombol clr ditekan, NS akan kembali ke S0
 			NS <= S0;
 		end if;
         case PS is
             when S0 =>
                 -- NW yellow and all others RED
-				N <= YELLOW; E <= RED;
-				W <= YELLOW; S <= RED;
+				N <= YELLOW; North <= "010";
+				E <= RED; 	 East  <= "001";
+				S <= RED; 	 South <= "001";
+				W <= YELLOW; West  <= "010";
 
                 Ts <= '1'; -- start timer
 				if(count = shortCount) then
@@ -87,8 +94,10 @@ begin --architecture
             
             when S1 =>
                 -- N - green, and all others RED
-				N <= GREEN; E <= RED; 
-				S <= RED;   W <= RED;
+				N <= GREEN; North <= "100";
+				E <= RED; 	East  <= "001";
+				S <= RED;	South <= "001";
+				W <= RED;	West  <= "001";
 				
                 Tl <= '1'; -- start timer		
 				if(count = longCount) then
@@ -97,9 +106,11 @@ begin --architecture
 				
             when S2 =>
                 -- NE yellow and all others RED
-                N <= YELLOW; S <= RED;
-				E <= YELLOW; W <= RED;
-
+                N <= YELLOW; North <= "010";
+				E <= YELLOW; East  <= "010";
+				S <= RED;	 South <= "001";
+				W <= RED;	 West  <= "001";
+				
                 Ts <= '1'; -- start timer
 				if(count = shortCount) then
 					NS <= S3;  -- Next State
@@ -107,8 +118,10 @@ begin --architecture
 				
             when S3 =>
                 -- E - green, and all others RED
-				E <= GREEN; N <= RED;
-				S <= RED;	W <= RED;
+				N <= RED;	North <= "001";
+				E <= GREEN; East  <= "100";
+				S <= RED;	South <= "001";
+				W <= RED;	West  <= "001";
 
                 Tl <= '1'; -- start timer
 				if(count = longCount) then
@@ -117,8 +130,10 @@ begin --architecture
 				
             when S4 =>
                 -- ES yellow and all others RED
-				E <= YELLOW; N <= RED;
-				S <= YELLOW; W <= RED;
+				E <= YELLOW; East  <= "010";
+				N <= RED;  	 North <= "001";
+				S <= YELLOW; South <= "010";
+				W <= RED; 	 West  <= "001";
 
                 Ts <= '1'; -- start timer
 				if(count = shortCount) then
@@ -127,8 +142,10 @@ begin --architecture
             
             when S5 =>
                 -- S - green, and all others RED
-				S <= GREEN; N <= RED;
-				E <= RED;	W <= RED;
+				S <= GREEN; South <= "100";
+				N <= RED; 	North <= "001";
+				E <= RED; 	East  <= "001";
+				W <= RED; 	West  <= "001";
 
                 Tl <= '1'; -- start timer
 				if(count = longCount) then
@@ -137,8 +154,10 @@ begin --architecture
 				
             when S6 =>
                 -- SW yellow and all others RED
-				S <= YELLOW; N <= RED;
-				W <= YELLOW; E <= RED;
+				S <= YELLOW; South <= "010";
+				N <= RED; 	 North <= "001";
+				W <= YELLOW; West  <= "010";
+				E <= RED; 	 East  <= "001";
 
                 Ts <= '1'; -- start timer
 				if(count = shortCount) then
@@ -147,8 +166,10 @@ begin --architecture
             
             when S7 =>
                 -- W - green, and all others RED
-				W <= GREEN; N <= RED;
-				E <= RED;	S <= RED;
+				W <= GREEN; West  <= "100";
+				N <= RED; 	North <= "001";
+				E <= RED; 	East  <= "001";
+				S <= RED; 	South <= "001";
 
                 Tl <= '1'; -- start timer
 				if(count = longCount) then
@@ -164,7 +185,7 @@ begin --architecture
     begin
         timeout <= '0';
 		if clr = '1' then
-			count <= 1;
+			count <= 1; -- when clr 1, counter will stay at 1.
 		
         elsif Tl = '1' then
 			if rising_edge(clk) then
